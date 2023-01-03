7ae4269fd747cc7389f47d66913beca8b933fc97
Jayden <929509323@qq.com>
2023-01-03 19:09:15 +0800

    Frontend WIP

---
+++ VegaEditor/GameProject/NewProject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VegaEditor.GameProject
+{
+    class NewProject : ViewModelBase
+    {
+        private string _name = "New Project";
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if(_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+    }
+
+
+
+}
