2877c66bbe03c5fccfe95ee21d6b2c4a85c412d3
Jayden <929509323@qq.com>
2023-03-14 16:12:41 +0800

    WIP window function implementations

--- Engine/Platform/Platform.h
+++ Engine/Platform/Platform.h
@@ -8,4 +8,14 @@ namespace vega::platform {
 
     window create_window(const window_init_info* const init_info = nullptr);
     void remove_window(window_id id);
+
+    void set_fullscreen(bool is_fullscreen) const;
+    bool is_fullscreen() const;
+    void* handle() const;
+    void set_caption(const wchar_t* caption) const;
+    const math::u32v4 size() const;
+    void resize(u32 width, u32 height) const;
+    const u32 width() const;
+    const u32 height() const;
+    bool is_closed() const;
 }
