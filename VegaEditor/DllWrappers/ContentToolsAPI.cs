6c9e97ae6a2082256db89ec928148876ade0fcb6
Jayden <929509323@qq.com>
2023-03-27 15:44:47 +0800

    LOD and Mesh Asset implementatin

---
+++ VegaEditor/DllWrappers/ContentToolsAPI.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Numerics;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Controls;
+using VegaEditor.ContentToolsAPIStructs;
+using VegaEditor.Utilities;
+
+namespace VegaEditor.ContentToolsAPIStructs
+{
+    [StructLayout(LayoutKind.Sequential)]
+    class GeometryImportSettings
+    {
+        public float SmoothingAngle = 178f;
+        public byte CalculateNormals = 0;
+        public byte CalculateTangents = 1;
+        public byte ReverseHandedness = 0;
+        public byte ImportEmbededTextures = 1;
+        public byte ImportAnimations = 1;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    class SceneData : IDisposable
+    {
+        public IntPtr Data;
+        public int DataSize;
+        public GeometryImportSettings importSettings = new GeometryImportSettings();
+
+        public void Dispose()
+        {
+            Marshal.FreeCoTaskMem(Data);
+            GC.SuppressFinalize(this);
+        }
+
+        ~SceneData()
+        {
+            Dispose();
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    class PrimitiveInitInfo
+    {
+        public Content.PrimitiveMeshType Type;
+        public int SegmentX = 1;
+        public int SegmentY = 1;
+        public int SegmentZ = 1;
+        public Vector3 Size = new Vector3(1f);
+        public int LOD = 0;
+    }
+
+
+}
+
+namespace VegaEditor.DllWrappers
+{
+    static class ContentToolsAPI
+    {
+        private const string _toolsDLL = "ContentTools.dll";
+        [DllImport(_toolsDLL)]
+        private static extern void CreatePrimitiveMesh([In, Out] SceneData data, PrimitiveInitInfo info);
+        public static void CreatePrimitiveMesh(Content.Geometry geometry, PrimitiveInitInfo info)
+        {
+            Debug.Assert(geometry != null);
+            using var sceneData = new SceneData(); // here we used "using"
+                                                   // so that sceneData will be gone when outside of the current scope and call
+                                                   // disposable by destructor.
+                                                   // (But why normal var won't do that?
+                                                   // Seems to be still allocated on stack without "using" keyword)
+            try
+            {
+                CreatePrimitiveMesh(sceneData, info);
+                Debug.Assert(sceneData.Data != IntPtr.Zero && sceneData.DataSize > 0);
+                var data = new byte[sceneData.DataSize];
+                Marshal.Copy(sceneData.Data, data, 0, sceneData.DataSize);
+                geometry.FromRawData(data);
+            }
+            catch (System.Exception ex)
+            {
+                Logger.Log(MessageType.Error, $"failed to create {info.Type} primitive mesh.");
+                Debug.WriteLine(ex.Message);
+            }
+        }
+    }
+}
