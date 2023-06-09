bd13d0e70ad41ca2f11f29a9f14ea4331e87313f
Liu Yu Jia <yu-jia.liu@ubisoft.com>
2023-06-09 15:40:50 +0800

    Create a Fork
    Create a Fork

--- VegaEditor/obj/x64/DebugEditor/Editors/WorldEditor/ProjectLayoutView.g.i.cs
+++ VegaEditor/obj/x64/DebugEditor/Editors/WorldEditor/ProjectLayoutView.g.i.cs
@@ -48,13 +48,13 @@ namespace VegaEditor.Editors {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.17.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/VegaEditor;component/editors/worldeditor/projectlayoutview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/VegaEditor;V1.0.0.0;component/editors/worldeditor/projectlayoutview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Editors\WorldEditor\ProjectLayoutView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -64,7 +64,7 @@ namespace VegaEditor.Editors {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.17.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
@@ -74,7 +74,7 @@ namespace VegaEditor.Editors {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.17.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1800:DoNotCastUnnecessarily")]
